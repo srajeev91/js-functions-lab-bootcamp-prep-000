5d506fa0afef543ce68c94b5f9d4fa1cac58ae9e
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-01-18 22:30:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ function happyHolidays () {
 }
 
 function happyHolidaysTo (name) {
-  return `Happy holidays, ${name}!`
+  return `Happy holidays, ${name}!`;
 }
 
 function happyHolidayTo (holiday, name) {
-  return `Happy ${holiday}, ${name}!`
+  return `Happy ${holiday}, ${name}!`;
 }
 function holidayCountdown (holiday, days) {
-  return 
+  return `It's ${days} days until ${holiday}!`;
 }
